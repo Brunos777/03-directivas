f07a1c017aead4a20280942a08851888590a6d57
Brunos777 <108486791+Brunos777@users.noreply.github.com>
2022-10-01 23:10:53 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <h1>directivas en vue.js</h1>
+<hr />
+<h2>v.text</h2>
+<span  v-text="'hola desde la directiva v-text'"></span>
+<br>
+<span>Hola desde la directiva v-text</span>
+<br>
+<span v-text="titulo"></span>
+<br>
+esta es una transmutacxion de texto con una variable prop <br>
+{{titulo}}
+</div>
+</template>
+<script>
+  export default{
+    data:() => ({
+      titulo:"hola desde vue.js",
+    }),
+  }
+</script>
+<style scoped>
+div{
+  text-align: center;
+   margin: 0px 2rem;
+}
+</style> 
